0f4eece2428e87187baad9fc31f3fc68b1b97edb
suwan-art <qdd5331@gmail.com>
2026-07-11 16:33:18 +0900

    Update app3.py

--- app3.py
+++ app3.py
@@ -28,19 +28,19 @@ with col1:
     top_color = st.select_slider("색상 톤", options=["밝음", "무난함", "어두움"], key="top_color")
 with col2:
     st.subheader("하의")
-    bottom_type = st.radio("종류", ["청바지", "슬랙스", "트레이닝 팬츠", "반바지"])
-    bottom_color = st.select_slider("핏(Fit)", options=["슬림", "레귤러", "오버핏"])
+    bottom_type = st.radio("종류", ["청바지", "슬랙스", "트레이닝 팬츠", "반바지"], key="bottom_type")
+    bottom_color = st.select_slider("핏(Fit)", options=["슬림", "레귤러", "오버핏"], key="bottom_color")
 
 st.header("디테일 추가")
 tab1, tab2 = st.tabs(["신발", "액세서리"])
 with tab1:
     st.write("오늘의 발걸음을 책임질 신발:")
-    shoes = st.selectbox("신발 선택", ["스니커즈", "운동화", "구두", "슬리퍼"])
+    shoes = st.selectbox("신발 선택", ["스니커즈", "운동화", "구두", "슬리퍼"], key="shoes")
     with st.expander("신발 선택 팁 보기"):
         st.info("너무 튀는 신발은 지양하도록 해요!")
 with tab2:
     st.write("포인트 아이템:")
-    acc = st.multiselect("액세서리 추가", ["모자", "안경", "목걸이", "가방"])
+    acc = st.multiselect("액세서리 추가", ["모자", "안경", "목걸이", "가방"], key="acc")
     with st.expander("액세서리 스타일링 팁 보기"):
         st.warning("너무 많은 액세서리는 투머치가 될 수 있어요.")
 
