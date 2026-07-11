a3706311817274146bb68ae35756e4705ca3ee34
suwan-art <qdd5331@gmail.com>
2026-07-11 15:06:45 +0900

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
 st.markdown("# 앱ui만들기")
-user_id = st.text_input("이름", placeholder="홍길동")
+user_id = st.text_input("이름", placeholder="이름")
 ai_model = st.radio("학년", ["1", "2", "3"], horizontal=True)
+user_id = st.text_input("반", placeholder="반")
