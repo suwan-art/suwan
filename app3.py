14163cbc66c708b7b61b4362634c595082b37724
suwan-art <qdd5331@gmail.com>
2026-07-11 16:29:51 +0900

    Update app3.py

--- app3.py
+++ app3.py
@@ -12,7 +12,7 @@ def reset_all ():
     
 with st.sidebar:
     st.header("프로필")
-    user_name = st.text_input("닉네임")
+    user_name = st.text_input("닉네임", key="user_name")
     weather = st.selectbox("오늘 날씨", ["맑음", "흐림", "비/눈", "매우 추움"])
     st.markdown("---")
     st.info(f"반가워요, {user_name}님! 오늘 날씨는 '{weather}'이네요.")
