e24767135964e451baddcaec692f25f9bab899bf
suwan-art <qdd5331@gmail.com>
2026-07-11 16:31:25 +0900

    Update app3.py

--- app3.py
+++ app3.py
@@ -13,7 +13,7 @@ def reset_all ():
 with st.sidebar:
     st.header("프로필")
     user_name = st.text_input("닉네임", key="user_name")
-    weather = st.selectbox("오늘 날씨", ["맑음", "흐림", "비/눈", "매우 추움"])
+    weather = st.selectbox("오늘 날씨", ["맑음", "흐림", "비/눈", "매우 추움"], key="weather")
     st.markdown("---")
     st.info(f"반가워요, {user_name}님! 오늘 날씨는 '{weather}'이네요.")
 
@@ -24,8 +24,8 @@ st.header("👕 아이템 조합하기")
 col1, col2 = st.columns(2)
 with col1:
     st.subheader("상의")
-    top_type = st.radio("종류", ["후드티", "셔츠", "맨투맨", "반팔 티셔츠"])
-    top_color = st.select_slider("색상 톤", options=["밝음", "무난함", "어두움"])
+    top_type = st.radio("종류", ["후드티", "셔츠", "맨투맨", "반팔 티셔츠"], key="top_type")
+    top_color = st.select_slider("색상 톤", options=["밝음", "무난함", "어두움"], key="top_color")
 with col2:
     st.subheader("하의")
     bottom_type = st.radio("종류", ["청바지", "슬랙스", "트레이닝 팬츠", "반바지"])
