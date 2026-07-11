b6a413e9fee2ec2c3efdde228051aa4eae31a3fe
suwan-art <qdd5331@gmail.com>
2026-07-11 15:13:06 +0900

    Update app2.py

--- app2.py
+++ app2.py
@@ -7,3 +7,6 @@ user_id = st.text_input("반", placeholder="반")
 ai_speed = st.select_slider("난이도",options=["쉬움", "보통", "어려움"],value="보통")
 creativity = st.slider("점수", 0, 100, 50)
 question = st.text_area("소감", placeholder="소감")
+if st.button("질문 전송하기"):
+    if agree:
+        st.success(f"(user_id),")
